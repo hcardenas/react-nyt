cf28504275446bee3ceffbc6177dcd22657ad16b
helmut cardenas <aleksey89col@gmail.com>
2017-11-13 17:18:03 -0500

    heroku build

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -6,7 +6,7 @@ export default class Nav extends Component {
 	render() {
 		return (
 			<Navbar brand={<span><Icon large>comment</Icon> NYT News</span>} right className="indigo darken-4 z-depth-4">
-				<NavItem href='/search'>Search</NavItem>
+				<NavItem href='/home'>Home</NavItem>
 				<NavItem href='/Saved'>Saved</NavItem>
 			</Navbar>
 		);
